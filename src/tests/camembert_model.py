9fb4729258d3d246722ef3acfe5a2695e62e5481
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-06-16 09:34:30 -0400

    testing models

---
+++ src/tests/camembert_model.py
@@ -0,0 +1,16 @@
+from sentence_transformers import SentenceTransformer
+model =  SentenceTransformer("dangvantuan/sentence-camembert-large")
+
+sentences = ["Un avion est en train de décoller.",
+          "Un homme joue d'une grande flûte.",
+          "Un homme étale du fromage râpé sur une pizza.",
+          "Une personne jette un chat au plafond.",
+          "Une personne est en train de plier un morceau de papier.",
+          ]
+
+embeddings = model.encode(sentences)
+
+for sentence, embedding in zip(sentences, embeddings):
+    print("Sentence:", sentence)
+    print("Embedding:", embedding)
+    print()
